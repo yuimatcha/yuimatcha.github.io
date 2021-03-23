620ff6939881cc384cea653b049a26d2bdde4c9c
yuimatcha <faridsaja12@gmail.com>
2021-03-23 21:14:45 +0700

    beb

---
+++ edit_isi_html.js
@@ -0,0 +1,9 @@
+var span = "Opss 404";
+var h4 = "PAGE NOT FOUND";
+var p = "Oops! Looks Like You Got Lost";
+var p_del = "Please Try Again Later";
+
+document.getElementById("spann").innerHTML = span;
+document.getElementById("h4").innerHTML = h4;
+document.getElementById("p_1").innerHTML = p;
+document.getElementById("p_2").innerHTML = p_del;
